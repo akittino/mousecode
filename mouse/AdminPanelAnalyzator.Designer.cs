752e31fab6576e99c366389a923506d24be1f44f
Dagmara Kotecka <dagkotecka@gmail.com>
2014-11-19 01:22:40 +0100

    gui changes in adminPanel

--- mouse/AdminPanelAnalyzator.Designer.cs
+++ mouse/AdminPanelAnalyzator.Designer.cs
@@ -29,28 +29,44 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.picture_box = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.checkBox2);
             this.panel1.Controls.Add(this.treeView1);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 527);
+            this.panel1.Size = new System.Drawing.Size(247, 604);
             this.panel1.TabIndex = 0;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(112, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Show Right";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(3, 26);
+            this.treeView1.Location = new System.Drawing.Point(3, 74);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(190, 494);
+            this.treeView1.Size = new System.Drawing.Size(237, 523);
             this.treeView1.TabIndex = 1;
             // 
             // checkBox1
@@ -63,27 +79,47 @@
             this.checkBox1.Text = "Show Left";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // picture_box
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(112, 3);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(81, 17);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Show Right";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.picture_box.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.picture_box.Location = new System.Drawing.Point(265, 11);
+            this.picture_box.Name = "picture_box";
+            this.picture_box.Size = new System.Drawing.Size(800, 600);
+            this.picture_box.TabIndex = 11;
+            this.picture_box.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // AdminPanelAnalyzator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 551);
+            this.ClientSize = new System.Drawing.Size(1077, 628);
+            this.Controls.Add(this.picture_box);
             this.Controls.Add(this.panel1);
             this.Name = "AdminPanelAnalyzator";
             this.Text = "AdminPanelAnalyzator";
             this.Load += new System.EventHandler(this.AdminPanelAnalyzator_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -94,5 +130,8 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox picture_box;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- mouse/AdminPanelAnalyzator.cs
+++ mouse/AdminPanelAnalyzator.cs
@@ -83,4 +83,9 @@ namespace mysz
                 }
             }
     }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            picture_box.InitialImage = null;
+        }
 }}
